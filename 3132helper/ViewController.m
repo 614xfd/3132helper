51f0402a80df1f2a1a412a95c6b288b4b0cd5e44
DAA <tuxiangxue@163.com>
2018-08-31 09:49:02 +0800

    提交基础代码
    提交基础代码

--- 3132helper/ViewController.m
+++ 3132helper/ViewController.m
@@ -17,6 +17,30 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    
+    [self creatMainBtn];
+}
+- (void)creatMainBtn{
+    
+    NSArray *titiles = @[@"1",@"2",@"3",@"4"];
+    for (int i = 0; i < 4; i ++) {
+        UIButton *btn = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+        btn.frame = CGRectMake(i % 2 == 0? 50:self.view.frame.size.width / 2.0 + 50, 100 + 120 * (i / 2), 100, 100);
+        btn.backgroundColor = [UIColor lightGrayColor];
+        btn.tag = 100 + i;
+        [btn addTarget:self action:@selector(btnClick:) forControlEvents:UIControlEventTouchUpInside];
+        [btn setTitle:titiles[i] forState:UIControlStateNormal];
+        [self.view addSubview:btn];
+    }
+    
+}
+
+- (void)btnClick:(UIButton *)btn{
+    NSArray *names = @[@"FirstViewController",@"SecondViewController",@"ThirdViewController",@"FourthViewController"];
+    Class c = NSClassFromString(names[btn.tag - 100]);
+    
+    [self.navigationController pushViewController:[c alloc] animated:YES];
 }
 
 
