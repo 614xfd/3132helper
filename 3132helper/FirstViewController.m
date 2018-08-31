76586221e6cfb6c0406c10e1abb905bb1b9f89a9
DAA <tuxiangxue@163.com>
2018-08-31 10:23:09 +0800

    t提交数据库，pch

--- 3132helper/FirstViewController.m
+++ 3132helper/FirstViewController.m
@@ -17,6 +17,11 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    self.title = @"安全相册";
+    
+    
+    
+    
 }
 
 - (void)didReceiveMemoryWarning {
