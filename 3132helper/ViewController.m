56cdcf0721b0e900f4cf003d1f0dc1b1d208ffa9
DAA <tuxiangxue@163.com>
2018-09-03 10:26:00 +0800

    上传一下网络图标

--- 3132helper/ViewController.m
+++ 3132helper/ViewController.m
@@ -15,6 +15,7 @@
 @implementation ViewController
 
 - (void)viewDidLoad {
+    
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     self.title = @"3132助手";
@@ -23,15 +24,29 @@
 }
 - (void)creatMainBtn{
     
-    NSArray *titiles = @[@"1",@"2",@"3",@"4"];
+    NSArray *titiles = @[@{@"title":@"安全相册",@"img":@"helper01"},
+                         @{@"title":@"安全相册",@"img":@"helper02"},
+                         @{@"title":@"安全相册",@"img":@"helper03"},
+                         @{@"title":@"安全相册",@"img":@"helper04"},];
     for (int i = 0; i < 4; i ++) {
         UIButton *btn = [UIButton buttonWithType:UIButtonTypeRoundedRect];
-        btn.frame = CGRectMake(i % 2 == 0? 50:self.view.frame.size.width / 2.0 + 50, 100 + 120 * (i / 2), 100, 100);
-        btn.backgroundColor = [UIColor lightGrayColor];
+        btn.frame = CGRectMake(i % 2 == 0? 50:self.view.frame.size.width / 2.0 + 50, 140 + 160 * (i / 2), 120, 140);
+//        btn.backgroundColor = [UIColor lightGrayColor];
         btn.tag = 100 + i;
         [btn addTarget:self action:@selector(btnClick:) forControlEvents:UIControlEventTouchUpInside];
-        [btn setTitle:titiles[i] forState:UIControlStateNormal];
+//        [btn setTitle:titiles[i] forState:UIControlStateNormal];
         [self.view addSubview:btn];
+        
+        NSDictionary *data = titiles[i];
+        
+        UIImageView *imgV = [[UIImageView alloc]initWithImage:[UIImage imageNamed:data[@"img"]]];
+        imgV.frame = CGRectMake(10, 0, 100, 100);
+        [btn addSubview:imgV];
+        
+        UILabel *titleLab = [[UILabel alloc]initWithFrame:CGRectMake(0, 110, 120, 30)];
+        titleLab.textAlignment = NSTextAlignmentCenter;
+        titleLab.text = data[@"title"];
+        [btn addSubview:titleLab];
     }
 }
 
